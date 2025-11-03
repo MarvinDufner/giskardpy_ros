aff038133e3f9a6539327ddb6a45e920c7f1fd66
maltehue <huerkamp@uni-bremen.de>
2025-11-03 12:56:38 +0100

    removed comment

--- giskardpy_ros/configs/fetch_world_config.py
+++ giskardpy_ros/configs/fetch_world_config.py
@@ -6,7 +6,7 @@ from semantic_digital_twin.world import World
 @dataclass
 class WorldWithFetchedRobot(WorldConfig):
     robot_world: World = None
-    robot_class: type[AbstractRobot] = None  # 👈 Type hint: expects a class, not instance
+    robot_class: type[AbstractRobot] = None
     robot: AbstractRobot = field(init=False, default=None)
 
     def setup_collision_config(self):
