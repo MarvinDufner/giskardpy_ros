f29e4f0f73eb76f44a1ed2b1c732de27e946b7e6
maltehue <122607802+maltehue@users.noreply.github.com>
2026-02-20 23:43:40 +0100

    Update tiago.py

--- giskardpy_ros/configs/iai_robots/tiago.py
+++ giskardpy_ros/configs/iai_robots/tiago.py
@@ -46,7 +46,11 @@ class TiagoVelocityInterface(RobotInterfaceConfig):
             "arm_right_7_joint",
             "head_1_joint",
             "head_2_joint",
-            "torso_lift_joint"
+            "torso_lift_joint",
+            "gripper_right_left_finger_joint",
+            "gripper_right_right_finger_joint",
+            "gripper_left_left_finger_joint",
+            "gripper_left_right_finger_joint"
         ]
         self.add_joint_velocity_group_controller(
             cmd_topic="/realtime_body_controller_real/command", connections=joints
